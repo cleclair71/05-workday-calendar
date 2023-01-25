2a94055be55240b47c779ba97536ab4eccfbf312
cass <76407605+cleclair71@users.noreply.github.com>
2023-01-25 18:26:18 -0500

    formatting and cleanup

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,3 @@
-//! not changing colours with the hours
-//! not saving to storage
-// TODO: timer 
-// TODO: 
-// TODO: 
-// TODO: 
-// TODO: 
 
 // Grabbing the element with the id of "currentDate" and storing it in the variable displayDay
 let displayDay = document.getElementById("currentDate");
@@ -14,18 +7,18 @@ displayDay.innerHTML = dayjs().format("dddd, MMMM D YYYY hh:mm A");
 let displayHour = dayjs().format("H");
 
 
- $(".saveBtn").click(function (event) {
+$(".saveBtn").click(function (event) {
   // Preventing the default action of the button from happening
-event.preventDefault();
-// Getting the value of the element with the class "time-display" that is a 
-// sibling of the button that was clicked, and storing it in the variable "values"
-var values = $(this).siblings(".description").val();
-// Getting the value of the id of the parent element of the button, splitting it by the "-" character, 
-// and storing the second element (the hour) in the variable "time"
-var time = $(this).parent().attr("id");
-// Setting the value of the local storage item with the key of "time" to the value of "values"
+  event.preventDefault();
+  // Getting the value of the element with the class "time-display" that is a 
+  // sibling of the button that was clicked, and storing it in the variable "values"
+  var values = $(this).siblings(".description").val();
+  // Getting the value of the id of the parent element of the button, splitting it by the "-" character, 
+  // and storing the second element (the hour) in the variable "time"
+  var time = $(this).parent().attr("id");
+  // Setting the value of the local storage item with the key of "time" to the value of "values"
 
-localStorage.setItem(time, values);
+  localStorage.setItem(time, values);
 });
 function rowColour() {
 
@@ -40,7 +33,7 @@ function rowColour() {
     if (timeDisplay < displayHour) {
       $(this).addClass("past");
       // $(this).children(".rows").addClass("present");
-      
+
       // If the current hour is less than the hour of the current element, 
       // remove the class "present" and add the class "future" to the current element
     } else if (timeDisplay === displayHour) {
@@ -58,25 +51,25 @@ function rowColour() {
 }
 
 rowColour();
-
+// the function rowColour will be called every 15 seconds 
 var interval = setInterval(rowColour, 15000);
 // set the values of text boxes on a page when it is loaded from localStorage
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-$("#clearBtn").click(function(event) {
+$("#clearBtn").click(function (event) {
   // prevents any default action associated with the click event from occurring
   event.preventDefault();
   // sets the value of all textareas on the page to an empty string
   $("textarea").val("");
   // clears all data stored in localStorage for the current page.
   localStorage.clear();
-  
+
 });
